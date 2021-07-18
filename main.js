be04518dffd5c6c923ed3799b85647560990b6d5
ExequielGonzalez <gonzalez-exequiel@hotmail.com>
2021-07-17 21:41:07 -0300

    now the image is updated periodically, and is fetch from /photo/last/:id to get the last one

--- main.js
+++ main.js
@@ -1,23 +1,67 @@
-async function fetchText() {
-  let response = await fetch("http://localhost:3000/photo/camera1");
+// // codigo magico
+// window.setVariableInterval = function (callbackFunc, timing) {
+//   var variableInterval = {
+//     interval: timing,
+//     callback: callbackFunc,
+//     stopped: false,
+//     runLoop: function () {
+//       if (variableInterval.stopped) return;
+//       var result = variableInterval.callback.call(variableInterval);
+//       if (typeof result == "number") {
+//         if (result === 0) return;
+//         variableInterval.interval = result;
+//       }
+//       variableInterval.loop();
+//     },
+//     stop: function () {
+//       this.stopped = true;
+//       window.clearTimeout(this.timeout);
+//     },
+//     start: function () {
+//       this.stopped = false;
+//       return this.loop();
+//     },
+//     loop: function () {
+//       this.timeout = window.setTimeout(this.runLoop, this.interval);
+//       return this;
+//     },
+//   };
 
-  console.log(response.status); // 200
-  console.log(response.statusText); // OK
+//   return variableInterval.start();
+// };
+// // codigo magico
+
+let timeoutDelay = 350;
+
+const fetchPhoto = async function () {
+  const response = await fetch("http://localhost:3000/photo/last/camera1");
 
   if (response.status === 200) {
-    console.log(response);
-    let data = await response.json();
-    console.log(data);
-    let image = document.createElement("img");
-    image.id = "theImg";
-    image.style = "width:100%";
-    image.src = `data:image/png;base64,${toBase64(data.photo.data)}`;
+    timeoutDelay = 350;
+    const data = await response.json();
+
+    let image = document.querySelector("#theImg");
+    if (image !== null) {
+      image.src = `data:image/png;base64,${toBase64(data.photo.data)}`;
+    } else {
+      image = document.createElement("img");
+      image.id = "theImg";
+      image.style = "width:100%";
+      image.src = `data:image/png;base64,${toBase64(data.photo.data)}`;
+    }
+
     document.querySelector(".container").append(image);
+  } else {
+    console.log(timeoutDelay);
+    timeoutDelay = 5000;
   }
-}
+  setTimeout(fetchPhoto, timeoutDelay);
+};
 
 function toBase64(arr) {
   return btoa(arr.reduce((data, byte) => data + String.fromCharCode(byte), ""));
 }
 
-fetchText();
+window.onload = function () {
+  setTimeout(fetchPhoto, timeoutDelay);
+};
